30be5f62c56e809cdb96916d8a6b5b018310c75a
Diana Bouquet <bouquetdiana@gmail.com>
2022-10-17 18:39:01 -0300

    css para mobile

---
+++ script/footer.js
@@ -0,0 +1,15 @@
+document.getElementById("idfooter").innerHTML= `
+    <div>
+        <h2>chos malal</h2>    
+        <h3>oficina de turismo</h3>
+    </div>
+    <div>
+        <h4>Félix San Martín 182, Chos Malal, Neuquén.</h4>
+        <h4>Horario de Atención de 8:00 a 15:00</h4>
+    </div>
+    <div>
+        <h4>Tel. Fijo (+54) 0299 4424089</h4>
+        <h4>WhatsApp +5492995556435</h4>
+        <h4>turismo@chosmalal.gob.ar</h4>
+    </div>
+`
---
+++ script/header.js
@@ -0,0 +1,15 @@
+document.getElementById("idheader").innerHTML = `
+<div class="cabecera">
+            <div class="logo">
+                <a href="index.html"><img src="imagenes/logo_chosmalal.png" alt="Logo Chos Malal"></a>
+            </div>
+            <div class="botonera">
+                <ul>
+                <li><a href="quehacer.html"><h4>Qué hacer</h4></a></li>
+                <li><a href="gastronomia.html"><h4>Gastronomía</h4></a></li>
+                <li><a href="alojamiento.html"><h4>Alojamiento</h4></a></li>
+                <li><a href="info.html"><h4>Información</h4></a></li>
+                </ul>
+            </div>
+        </div>
+        `
